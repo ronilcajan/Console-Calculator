f0a733e917f1c26032b56ca6db64d59aae00b58c
Ronc122 <cajanr02@gmail.com>
2019-02-07 18:19:08 +0800

    Console Calculator

---
+++ MT_05_Cajan/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MT_05_Cajan
+{
+    class Program
+    {
+        public static int Sum(int num1, int num2)
+        {
+            int total;
+            total = num1 + num2;
+            return total;
+        }
+        public static int Substract(int num1, int num2)
+        {
+            int difference;
+            difference = num1 - num2;
+            return difference;
+        }
+        public static int Multiply(int num1, int num2)
+        {
+            int product;
+            product = num1 * num2;
+            return product;
+        }
+        public static int Divide(int num1,int num2)
+        {
+            int qoutient;
+            qoutient = num1 / num2;
+            return qoutient;
+        }
+        public static int Module(int num1, int num2)
+        {
+            int module;
+            module = num1 % num2;
+            return module;
+        }
+        static void Main(string[] args)
+        {
+            while(true)
+            {
+                String choice;
+                Console.WriteLine("SIMPLE CALCULATOR IN C#");
+                Console.WriteLine("[A] - ADDITION");
+                Console.WriteLine("[B] - SUBSTRACTION");
+                Console.WriteLine("[C] - MULTIPLICATION");
+                Console.WriteLine("[D] - DIVISION");
+                Console.WriteLine("[E] - MODULE");
+                Console.WriteLine("[X] - EXIT");
+
+                Console.Write("Enter your choice : ");
+                choice = Console.ReadLine();
+
+                if (choice == "a" || choice == "A")
+                {
+                    Console.Write("Enter first number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The Sum of two number is : {0} ", Sum(num1,num2));
+                }
+                else if (choice == "b" || choice == "B")
+                {
+                    Console.Write("Enter first number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The Difference of two number is : {0} ", Substract(num1, num2));
+                }
+                else if (choice == "c" || choice == "C")
+                {
+                    Console.Write("Enter first number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The Product of two number is : {0} ", Multiply(num1, num2));
+                }
+                else if (choice == "d" || choice == "D")
+                {
+                    Console.Write("Enter first number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The Quotient of two number is : {0} ", Divide(num1, num2));
+                }
+                else if (choice == "e" || choice == "E")
+                {
+                    Console.Write("Enter first number : ");
+                    int num1 = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Enter second number : ");
+                    int num2 = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("The Module of two number is : {0} ", Module(num1, num2));
+                }
+                else if (choice == "x" || choice == "X")
+                {
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Input!");
+                }
+            }
+        }
+    }
+}
---
+++ MT_05_Cajan/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MT_05_Cajan")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("TEAM OS")]
+[assembly: AssemblyProduct("MT_05_Cajan")]
+[assembly: AssemblyCopyright("Copyright © TEAM OS 2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a51d984e-df5a-489c-ad1e-7f00b520a2b4")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
